b65b4b638fdd5b8d8cd3cf2325cb94f67937307b
Eric Cheng <gn1692401@yahoo.com.tw>
2021-11-28 18:19:45 +0800

    add default config

--- lib/line_gundam.rb
+++ lib/line_gundam.rb
@@ -1,8 +1,30 @@
 # frozen_string_literal: true
 
-require_relative "line_gundam/version"
+require_relative 'line_gundam/version'
 
 module LineGundam
+  class Configuration
+    BROADCAST_URL = 'https://api.line.me/v2/bot/message/broadcast'
+
+    attr_accessor :access_token
+    attr_accessor :broadcast_url
+
+    def initialize
+      @access_token = ''
+      @broadcast_url = BROADCAST_URL
+    end
+  end
+
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      block_given? ? yield(configuration) : configuration
+    end
+  end
+
+
   class Error < StandardError; end
-  # Your code goes here...
 end
